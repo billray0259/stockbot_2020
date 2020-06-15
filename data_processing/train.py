7bc1a1ec08778e8c176e2a0601a1458a46fd35ed
bill0259 <bill.ray0259@gmail.com>
2020-06-15 00:00:31 -0400

    added requirements.txt, might be incomplete

--- data_processing/train.py
+++ data_processing/train.py
@@ -7,6 +7,7 @@ import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
 
 TRAINING_DATA = "/home/bill/stockbot_2020/processed_data/small_cap_example.npy"
+MODEL_DIR = "/home/bill/stockbot_2020/models"
 
 examples = np.load(TRAINING_DATA, allow_pickle=True)
 print(examples.shape)
@@ -28,6 +29,5 @@ model.compile(loss="categorical_crossentropy", optimizer="adam",
 
 epochs = 10
 model.fit(x, y, epochs=epochs, batch_size=256, validation_split=0.2)
-model.save("overfitmodel_" + str(epochs))
-# print(len(examples[0][1][1]))
+model.save("model")
 

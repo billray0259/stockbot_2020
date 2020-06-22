8d8e2113d8338a4e8c86edaddbddcd5008bb1a7a
bill0259 <bill.ray0259@gmail.com>
2020-06-22 16:46:19 -0400

    Created multi_symbol_binary_classification() function

--- data_handler.py
+++ data_handler.py
@@ -25,12 +25,9 @@ class DataHandler:
         self.finviz_file = os.path.join(self.data_dir, "finviz.h5")
 
         self.histories_dir = os.path.join(self.data_dir, "histories")
-        if not os.path.exists(self.histories_dir):
-            os.mkdir(self.histories_dir)
+        
         
         self.filled_histories_dir = os.path.join(self.data_dir, "filled_histories")
-        if not os.path.exists(self.filled_histories_dir):
-            os.mkdir(self.filled_histories_dir)
 
         self.joined_file = os.path.join(self.data_dir, "joined.h5")
         self.filled_file = os.path.join(self.data_dir, "filled.h5")
@@ -82,6 +79,9 @@ class DataHandler:
         df.to_hdf(self.finviz_file, "df", "w")
 
     def collect_histories(self, frequency=15, frequency_type="minute", days=365):
+        if not os.path.exists(self.histories_dir):
+            os.mkdir(self.histories_dir)
+        
         account = Account(self.keys_json)
 
         finviz_df = pd.read_hdf(self.finviz_file)
@@ -100,6 +100,9 @@ class DataHandler:
                 history_df.to_hdf(ticker_history_save_file, "df", "w")
 
     def join_candles(self, save_to_file=True, filled=False):
+        if not os.path.exists(self.filled_histories_dir):
+            os.mkdir(self.filled_histories_dir)
+        
         dataframes = []
         if filled:
             histories_dir = self.filled_histories_dir
@@ -206,42 +209,6 @@ class DataHandler:
         print("Saving to %s....\t%d" % (self.groups_file, time.time()))
         with open(self.groups_file, "wb") as groups_file:
             pickle.dump(means, groups_file)
-        
-
-    def preprocess(self, example_length=64, save_to_file=True):
-        candles = pd.read_hdf(self.joined_file, index_col="datetime")
-        all_examples = []
-        all_labels = []
-        tickers = set(
-            map(lambda column: column.split("_")[0], candles.columns))
-        closes = candles.filter(like="close")
-        for i in tqdm(range(example_length, len(candles)-1), desc="Preprocessing candles from %s" % self.joined_file):
-            examples = candles.iloc[i-example_length:i]
-            next_closes = closes.iloc[i+1]
-
-            means = np.mean(examples)
-            stds = np.std(examples)
-            examples = (examples - means) / stds
-            next_closes = (next_closes - means.filter(like="close")) / stds.filter(like="close")
-
-            for ticker in tickers:
-                example = examples.filter(regex="^%s_" % ticker)
-                close_column = ticker + "_close"
-                close = example[close_column].values[-1]
-                next_close = next_closes[close_column]
-                label = np.zeros(2)
-                label[int(close < next_close)] = 1
-                all_examples.append(example.to_numpy())
-                all_labels.append(label)
-
-        examples, labels = np.array(all_examples), np.array(all_labels)
-        if save_to_file:
-            np.save(examples, self.examples_file)
-            np.save(labels, self.labels_file)
-            print("Saved training data to %s and %s" %
-                  (self.examples_file, self.labels_file))
-        else:
-            return examples, labels
 
     def handle_data(self):
         self.save_finviz()
--- preprocessors.py
+++ preprocessors.py
@@ -1,49 +1,63 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import pickle
-from datetime import timedelta
+from tqdm import tqdm
+import os
 
 from data_handler import DataHandler
 
 
-def multi_symbol_binary_classification(data_handler):
+def multi_symbol_binary_classification(name, num_groups=1, candles_per_example=64):
+    data_handler = DataHandler(name)
+
+    finviz = pd.read_hdf(data_handler.finviz_file)
+    filled = pd.read_hdf(data_handler.filled_file)
+
     with open(data_handler.groups_file, "rb") as groups_file:
         groups = pickle.load(groups_file)
     
     sorted_keys = sorted(groups.keys(), reverse=True)
-    group = groups[sorted_keys[1]]
+    print("Preprocessing multi_symbol_binary_classification from" + data_handler.filled_file)
+    for highest_corr in tqdm(sorted_keys[:num_groups]):
+        tickers = groups[highest_corr]
+        biggest_ticker = max(tickers, key=lambda ticker: finviz["Market Cap"][ticker])
 
-    finviz = pd.read_hdf(data_handler.finviz_file)
-    not_etf = []
-    names = []
-    for ticker in group:
-        if finviz["Industry"][ticker] != "Exchange Traded Fund":
-            not_etf.append(ticker)
-            names.append(finviz["Company"][ticker])
+        columns = []
+        for ticker in tickers:
+            columns.extend([ticker + "_open", ticker + "_high", ticker + "_low", ticker + "_close", ticker + "_volume"])
         
-    print(not_etf)
-    print(len(not_etf))
-            
+        tickers_df = filled[columns]
 
-    filled = pd.read_hdf(data_handler.filled_file)
-    closes = filled.filter(like="_close")
-    closes.columns = list(map(lambda column: column.split("_")[0], closes.columns))
-    group_prices = closes[not_etf]
-    group_prices.index = pd.to_datetime(group_prices.index)
-    df = group_prices.resample("D").mean().dropna()
-    df = df/df.mean()
+        x = []
+        y = []
+        closes = tickers_df.filter(like="_close")
+        for i in tqdm(range(candles_per_example, len(tickers_df)-1)):
+            example = tickers_df.iloc[i-candles_per_example:i]
+            next_close = closes.iloc[i+1]
 
-    for key in df:
-        plt.plot(df[key])
-    plt.legend(names)
-    plt.show()
-    
-    
+            means = np.mean(example)
+            stds = np.std(example)
+            example = (example - means) / stds
+            next_close = (next_close - means.filter(like="_close")) / stds.filter(like="close")
+            this_close = example.filter(like="_close").iloc[-1]
+            label = this_close < next_close
+            label = np.array(list(map(lambda b: [0, 1] if b else [1, 0], label.to_numpy())))
+            example = example.to_numpy()
+
+            x.append(example)
+            y.append(label)
 
 
+        x, y = np.array(x), np.array(y)
 
+        dir_path = os.path.join(data_handler.data_dir, "msbc_training", biggest_ticker + "_group")
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)
+        
+        x_file = os.path.join(dir_path, "x.npy")
+        y_file = os.path.join(dir_path, "y.npy")
+        np.save(x_file, x)
+        np.save(y_file, y)
 
 if __name__ == "__main__":
-    dh = DataHandler("all")
-    multi_symbol_binary_classification(dh)
+    multi_symbol_binary_classification("stocks_only", num_groups=3)

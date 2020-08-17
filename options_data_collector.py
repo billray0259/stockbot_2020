3b5cf3d4a083074c5e421bee5d86a597b919a3b5
bill0259 <bill.ray0259@gmail.com>
2020-08-17 12:05:51 -0400

    options_data_collector.py tweeks, and saving before refactoring

--- options_data_collector.py
+++ options_data_collector.py
@@ -1,5 +1,4 @@
 from datetime import datetime, timedelta
-from data_handler import DataHandler
 import pandas as pd
 import numpy as np
 import td_api as td
@@ -10,6 +9,7 @@ import sys
 import math
 from multiprocessing import Pool
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 #NUM_TICKERS = 100
 STRIKE_COUNT = 50
@@ -21,9 +21,13 @@ TIME_MOD_EST = 0
 
 SAVE_DIR = "options_data"
 
-def divide_chunks(l, n): 
-    for i in range(0, len(l), n):  
-        yield l[i:i + n] 
+account = td.Account("keys.json")
+
+
+def divide_chunks(l, n):
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
+
 
 def get_monthly_expiration_dates(num_dates):
     expiration_days = []
@@ -47,8 +51,9 @@ def get_monthly_expiration_dates(num_dates):
                 day = datetime(year=day.year, month=day.month+1, day=1)
         else:
             day += delta
-    
+
     return expiration_days
+
 
 def get_fridays(startDate):
     d = startDate
@@ -62,25 +67,37 @@ def get_fridays(startDate):
     return fridays
 
 
-account = td.Account("keys.json")
-
 def initalize():
+    os.chdir(os.path.dirname(__file__))
     if not path.exists('options_data'):
+        print("Creating options_data directory")
         os.mkdir('options_data')
 
+
 def update_primary():
-    driver = webdriver.Chrome()
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    driver = webdriver.Chrome(options=chrome_options)
+    print("Loading barchart.com")
     driver.get('https://www.barchart.com/options/volume-leaders/stocks?page=all')
-    time.sleep(70)
-    tickers = []
-    for x in range(1, 750):
-        tickers.append(driver.find_element_by_xpath('/html/body/div[2]/div/div[2]/div[2]/div/div/div/div/div/div[5]/div/div[2]/div/div/ng-transclude/table/tbody/tr['+str(x)+']/td[1]/div').text)
-    tickers = list(set(tickers))[:100]
-    now = datetime.now()
+    xpath = '/html/body/div[2]/div/div[2]/div[2]/div/div/div/div/div/div[5]/div/div[2]/div/div/ng-transclude/table/tbody/tr[%d]/td[1]/div'
+    while len(driver.find_elements_by_xpath(xpath % 1)) == 0:
+        time.sleep(0.25)
+    print("Collecting tickers")
+    tickers = set([])
+    for i in range(1, 750):
+        ticker = driver.find_element_by_xpath(xpath % i).text
+        print(ticker + "\t%.0f%%" % (len(tickers)), end="\r", flush=True)
+        tickers.add(ticker)
+        if len(tickers) == 100:
+            break
+    now = datetime.now().strftime("%Y-%m-%d")
     df = pd.DataFrame(columns=['Time', 'Ticker'])
     for ticker in tickers:
         df = df.append(pd.DataFrame(data={'Time': [now], 'Ticker': [ticker]}), ignore_index=True)
+    print("Saving tickers to primary_tickers.csv")
     df.to_csv('options_data/primary_tickers.csv')
+
 
 def update_secondary():
     if not path.exists('options_data/primary_tickers.csv'):
@@ -99,8 +116,9 @@ def update_secondary():
             dropped_tickers.append((time, ticker))
     for item in dropped_tickers:
         primary_time_tickers.append(item)
-    df = pd.DataFrame(primary_time_tickers, columns =['Time', 'Ticker']) 
+    df = pd.DataFrame(primary_time_tickers, columns=['Time', 'Ticker'])
     df.to_csv('options_data/secondary_tickers.csv')
+
 
 def gather_symbols():
     symbols = []
@@ -109,9 +127,9 @@ def gather_symbols():
     secondary = pd.read_csv('options_data/secondary_tickers.csv')
     tickers = list(zip(secondary['Time'], secondary['Ticker']))
     for timestamp, ticker in tickers:
-        from_date = datetime.strptime(timestamp, '%Y-%m-%d %H:%M:%S.%f')
-        to_date = max(get_fridays(from_date)) 
-        if datetime.now()-to_date > 0:
+        from_date = datetime.strptime(timestamp, '%Y-%m-%d')
+        to_date = max(get_fridays(from_date))
+        if datetime.now() < to_date:
             time.sleep(max(0.001, 0.6 - (time.time() - last_request_time)))
             last_request_time = time.time()
             options_chain = account.get_options_chain(ticker, from_date=from_date, to_date=to_date, strike_count=STRIKE_COUNT)
@@ -121,6 +139,7 @@ def gather_symbols():
     with open("options_data/symbols.txt", "w+") as symbols_file:
         print("Writing Symbols")
         symbols_file.write("\n".join(symbols))
+
 
 def collect_data():
     with open("options_data/symbols.txt", "r") as symbols_file:
@@ -142,21 +161,23 @@ def collect_data():
                 quotes = None
                 print('Collected ' + str(x))
                 time.sleep(6)
-            string = str(datetime.now()).replace(" ", '-').replace(':', '-').split('.')[0] + '.csv'
-            master_df.to_csv('options_data/' + string)
-            print('Collected Data For ' + string)
+            file_name = datetime.now().strftime("%Y-%m-%d_%H:%M:%S") + ".csv"
+            master_df.to_csv('options_data/' + file_name)
+            print('Collected Data For ' + file_name)
+
 
 if __name__ == '__main__':
     initalize()
-    if sys.argv[1] == 'update':
-        if sys.argv[2] == 'primary':
-            update_primary()
-        elif sys.argv[2] == 'secondary':
-            update_secondary()
+    if sys.argv[1] == 'update': # Run saturdays 9:30am
+        update_primary()
+        update_secondary()
+        # if sys.argv[2] == 'primary':
+        #     update_primary()
+        # elif sys.argv[2] == 'secondary':
+        #     update_secondary()
 
-    elif sys.argv[1] == "list":
+    elif sys.argv[1] == "list": # Run weekdays at 9:00am
         gather_symbols()
 
-    elif sys.argv[1] == 'collect':
+    elif sys.argv[1] == 'collect': # Run weekdays at 9:30am
         collect_data()
-        
--- test.py
+++ test.py
@@ -1,13 +1,7 @@
-from td_api import Account
-from datetime import datetime, timedelta
+import pandas as pd
 
-acc = Account("keys.json")
+df = pd.read_csv("temp.csv")
 
+df = df[["underlying", "delta", "strikePrice"]]
 
-symbol = "AMD"
-days = 30
-strike_count = 50
-
-from_date = datetime.now()
-to_date = from_date + timedelta(days=days)
-data = acc.get_options_chain(symbol, from_date, to_date, strike_count=strike_count)
+print(df)
